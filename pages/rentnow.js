4a5d1309b6aefd5c6c8eb7f2a33c7520feba494a
oliverloops <oliveralr@protonmail.com>
2021-03-19 14:35:35 -0600

    Adding API functionality

--- pages/rentnow.js
+++ pages/rentnow.js
@@ -1,19 +1,28 @@
+import useSWR from "swr";
 import { Box, Text, Flex, Heading, Spacer } from "@chakra-ui/react";
 //Custom UI components
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-const RentNow = ({ props }) => {
-  // console.log(props);
-
+// Lift up content
+const RentNow = (props) => {
   return (
     <>
       <Navbar position={"relative"} background={"#1687a7"} />
-      <Flex p="8">
-        <Text>Here's the lisitng...</Text>
-      </Flex>
+      <Listing data={props.res} />
       <Footer />
     </>
+  );
+};
+
+//Move state down
+const Listing = ({ data }) => {
+  console.log(data);
+
+  return (
+    <Flex p="8">
+      <Text>Here's the listing in other component...</Text>
+    </Flex>
   );
 };
 
@@ -21,11 +30,12 @@ export async function getStaticProps() {
   const data = await fetch("http://localhost:3000/api/hello");
   const res = await data.json();
 
-  console.log(res);
   return {
     props: {
       res,
     },
+    // Incremental Static Regeneration
+    revalidate: 1,
   };
 }
 
