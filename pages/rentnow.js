273b65f5e17babf8e2077804b94ca4730ea348df
oliverloops <oliveralr@protonmail.com>
2021-03-19 23:59:58 -0600

    Adding API functionality

--- pages/rentnow.js
+++ pages/rentnow.js
@@ -1,25 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useContext, createContext } from "react";
 import useSWR from "swr";
 import { Box, Text, Flex, Heading, Spacer } from "@chakra-ui/react";
 //Custom UI components
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
+//Context API
+const listingContext = createContext();
+
 // Lift up content
 const RentNow = ({ listing }) => {
   return (
     <>
       <Navbar position={"relative"} background={"#1687a7"} />
-      <Listing list={listing} />
+      <listingContext value={listing}>
+        <Listing />
+      </listingContext>
       <Footer />
     </>
   );
 };
 
+// Fetcher wrapper for data fetching (swr)
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 //Move state down
-const Listing = ({ list }) => {
-  const { data } = useSWR("/api/lisiting", fetcher, { initialData: list });
-  console.log(data);
+const Listing = () => {
+  // const { data } = useSWR("/api/lisiting", fetcher, { initialData: list });
+  const list = useContext(listingContext);
+  console.log(list);
 
   return (
     <Flex p="8">
@@ -29,9 +38,9 @@ const Listing = ({ list }) => {
 };
 
 export async function getStaticProps() {
-  // Fetcher wrapper for data fetching (swr)
-  const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const listing = fetcher("/api/listing");
+  const res = await fetch("http://localhost:3000/api/listing");
+  const listing = await res.json();
+  // const listing = await fetcher("/api/listing");
 
   return {
     props: {
